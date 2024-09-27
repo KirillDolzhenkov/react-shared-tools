2e6d87b4189f56392aee91b9400efd427adcf9ce
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2024-09-27 21:27:54 +0300

    fixed useObserverInfiniteScroll & useInfiniteScroll description

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -5,6 +5,28 @@ export interface IUseInfiniteScroll {
   distanceToBottom?: number;
 }
 
+/**
+ * Хук для реализации бесконечной прокрутки.
+ *
+ * @param {IUseInfiniteScroll} props - Объект с параметрами для настройки бесконечной прокрутки.
+ * @param {Function} [props.callBack] - Функция, которая будет вызвана, когда пользователь приблизится к нижней части страницы.
+ * @param {number} [props.distanceToBottom=100] - Расстояние от нижней части страницы, при котором будет вызван callBack. По умолчанию 100 пикселей.
+ *
+ * @example
+ * const MyComponent = () => {
+ *   useInfiniteScroll({
+ *     callBack: () => console.log('Reached the bottom!'),
+ *     distanceToBottom: 200
+ *   });
+ *
+ *   return (
+ *     <div>
+ *       <p>Scroll down to see the magic happen!</p>
+ *     </div>
+ *   );
+ * };
+ */
+
 export const useInfiniteScroll = (props: IUseInfiniteScroll) => {
   const { callBack, distanceToBottom = 100 } = props;
 
