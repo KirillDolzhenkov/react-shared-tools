4e3e17f59440c40c4f9cf896af5c41ced16ac942
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2025-03-27 23:51:39 +0300

    feat: added isMounted hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -4,3 +4,4 @@ export { default as infiniteObserverScroll } from './infiniteObserverScroll';
 export { default as infiniteScroll }         from './infiniteScroll';
 export { default as useResize }              from './resizeObserver';
 export { default as useWindowWidth }         from './windowWidth';
+export { default as useIsMounted }           from './isMounted';
